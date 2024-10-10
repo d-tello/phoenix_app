73516134830db0fa315c245db149c77bf8b20263
Davide Tello <tello.davide@gmail.com>
2024-10-10 10:47:16 +0200

    Add seed data for CollectionRequest model

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhoenixApp.Repo
+alias PhoenixApp.CollectionRequest
+
+# Define seed data
+collectionRequests = [
+  %CollectionRequest{
+    ref_url: "https://petsdeli.de/1",
+    slug: "example-slug-1",
+    segments: ["segment1", "segment2"],
+    audiences: ["audience1", "audience2"],
+    pet_type: "dog"
+  },
+  %CollectionRequest{
+    ref_url: "https://petsdeli.de/2",
+    slug: "example-slug-2",
+    segments: ["segment3", "segment4"],
+    audiences: ["audience3", "audience4"],
+    pet_type: "cat"
+  }
+]
+
+# Insert seed data into the database
+Enum.each(collectionRequests, fn collectionRequest ->
+  Repo.insert!(collectionRequest)
+end)

5d5e8d988363e01dc2c61f88f81a54c7bfac8243
Davide Tello <tello.davide@gmail.com>
2024-10-10 13:08:07 +0200

    Add seed data for CollectionRequest model

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhoenixApp.Repo
+alias PhoenixApp.Collections.Log
+
+logs = for _ <- 1..20 do
+  %Log{
+    ref_url: "http://petsdeli.de/#{:rand.uniform(1000)}",
+    slug: "slug-#{:rand.uniform(1000)}",
+    segments: ["segment1", "segment2"],
+    audiences: ["audience1", "audience2"],
+    pet_type: Enum.random(["dog", "cat",])
+  }
+end
+
+Enum.each(logs, fn log ->
+  Repo.insert!(log)
+end)
